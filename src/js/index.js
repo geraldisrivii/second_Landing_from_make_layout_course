5192ad6241241e29033cb5594211b3c7e51e134a
Alexander Maltsev <aleksander.freelancer@gmail.com>
2023-02-05 11:22:33 +0600

    Create card

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,19 @@
+// Define elements
 let list = document.querySelector(".grid-box")
 let header = document.querySelector('.button')
-let property = "change"
-console.log(header.hasAttribute(property) ? header.getAttribute(property) : "Propery isn't existing")
-for (let index = 0; index < 2; index++) {
+// Append element to list
+for (let index = 0; index < 4; index++) {
     let element = list.firstChild.cloneNode(true)
     element.firstChild.src = `icons/Vector-${index}.svg`
     console.log(element.firstChild.src)
     list.append(element)
 }
-let width = window.innerWidth
-for (const item of list.children){
-  item.classList.add('grid-box-element_span_2')
-}
 let quantity_of_columns = getComputedStyle(list).gridTemplateColumns.split(" ").length
+if(list.children.length > 4 & quantity_of_columns % 2 == 0 & quantity_of_columns > 3) {
+  for (const item of list.children) {
+    item.style.gridColumn = "span 2"
+  }
+}
 console.log(quantity_of_columns)
 if(list.children.length % 2 == 0 & quantity_of_columns % 2 > 0){
     list.lastChild.style.gridColumn = "span 2"
